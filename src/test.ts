8f6ec0059b22494a9c12a57e6f5170f59e79e980
Blair Strang <Blair.Strang@auspost.com.au>
2016-01-18 10:04:52 +1100

    1.0.0

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,46 @@
-// import { expect } from 'chai';
+'use strict';
 
-/*
+import * as http from 'http';
+import { IService, Service, ServiceGroup } from './index';
 
-import * as fs from './index';
+class FailingService implements IService {
+    start(): Promise<void> {
+        return Promise.reject('Aieeeeee');
+    }
+    stop(): Promise<void> {
+        return Promise.reject('IT BURNS');
+    }
+}
 
-describe('fs', function() {
-    describe('unlink', function() {
-        it('should do maybe unlink stuff', function(done) {
-            fs.writeFileSync('/tmp/foo', 'blah');
-            fs.unlinkAsync('/tmp/foo').then(function() {
-                done();
-            }).catch(done);
+describe('service', function() {
+    it('should start/stop a service', function(done) {
+        var s = new Service(http.createServer(), { port: 31337 });
+        s.start().then(function() {
+            return s.stop();
+        }).then(function() {
+            done();
+        }).catch(done);
+    });
+    it('should start/stop a service group', function(done) {
+        var sg = new ServiceGroup([
+            new Service(http.createServer(), { port: 31337 }),
+            new Service(http.createServer(), { port: 31338 })
+        ]);
+        sg.start().then(function () {
+            return sg.stop();
+        }).then(function () {
+            done();
+        }).catch(done);
+    });
+    it('service group should fail on failure of a service', function(done) {
+        var sg = new ServiceGroup([
+            new Service(http.createServer(), { port: 31337 }),
+            new FailingService()
+        ]);
+        sg.start().then(function () {
+            done(Error('It should not have worked'));
+        }).catch(function (err) {
+            done();
         });
     });
 });
-
-*/
