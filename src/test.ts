e2f79e9a7e859fecc5031f5945785617fca1e8cc
bls <blair.strang@gmail.com>
2016-01-18 19:49:47 +1100

    Go full retard with fat-arrow, because 0.12 is a write-off anyway.

--- src/test.ts
+++ src/test.ts
@@ -12,34 +12,34 @@ class FailingService implements IService {
     }
 }
 
-describe('service', function() {
-    it('should start/stop a service', function(done) {
+describe('service', () => {
+    it('should start/stop a service', (done) => {
         var s = new Service(http.createServer(), { port: 31337 });
-        s.start().then(function() {
+        s.start().then(() => {
             return s.stop();
-        }).then(function() {
+        }).then(() => {
             done();
         }).catch(done);
     });
-    it('should start/stop a service group', function(done) {
+    it('should start/stop a service group', (done) => {
         var sg = new ServiceGroup([
             new Service(http.createServer(), { port: 31337 }),
             new Service(http.createServer(), { port: 31338 })
         ]);
-        sg.start().then(function () {
+        sg.start().then(() => {
             return sg.stop();
-        }).then(function () {
+        }).then(() => {
             done();
         }).catch(done);
     });
-    it('service group should fail on failure of a service', function(done) {
+    it('service group should fail on failure of a service', (done) => {
         var sg = new ServiceGroup([
             new Service(http.createServer(), { port: 31337 }),
             new FailingService()
         ]);
-        sg.start().then(function () {
+        sg.start().then(() => {
             done(Error('It should not have worked'));
-        }).catch(function (err) {
+        }).catch((err) => {
             done();
         });
     });
